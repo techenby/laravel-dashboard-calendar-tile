8e3539ca52565656c7b497a6010c2d99cd9fb376
Andy Newhouse <hi@andymnewhouse.me>
2022-06-04 10:41:24 -0500

    Allow for joining calendars

--- src/CalendarTileComponent.php
+++ src/CalendarTileComponent.php
@@ -9,6 +9,9 @@ class CalendarTileComponent extends Component
     /** @var string */
     public $calendarId;
 
+    /** @var array */
+    public $calendarIds;
+
     /** @var string */
     public $position;
 
@@ -18,9 +21,9 @@ class CalendarTileComponent extends Component
     /** @var int|null */
     public $refreshInSeconds;
 
-    public function mount(?string $calendarId = null, string $position, ?string $title = null, int $refreshInSeconds = null)
+    public function mount($calendarId = null, string $position, ?string $title = null, int $refreshInSeconds = null)
     {
-        $this->calendarId = $calendarId ?? config('dashboard.tiles.calendar.ids')[0];
+        $this->calendarId = $calendarId;
 
         $this->position = $position;
 
@@ -32,7 +35,9 @@ class CalendarTileComponent extends Component
     public function render()
     {
         return view('dashboard-calendar-tile::tile', [
-            'events' => CalendarStore::make()->eventsForCalendarId($this->calendarId),
+            'events' => is_array($this->calendarId)
+                ? CalendarStore::make()->eventsForCalendarIds($this->calendarId)
+                : CalendarStore::make()->eventsForCalendarId($this->calendarId),
             'refreshIntervalInSeconds' => $this->refreshInSeconds ?? config('dashboard.tiles.calendar.refresh_interval_in_seconds') ?? 60,
         ]);
     }
